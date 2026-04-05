00d2c80f0bf309b3f1a73720bd38439cdfb1ece2
Madison Rye Progress <makyo@drab-makyo.com>
2026-04-05 14:09:11 -0700

    Switch to interface{} for completion keys

--- term.go
+++ term.go
@@ -94,13 +94,22 @@ type TermNameOptions struct {
 }
 
 type TermCompletion struct {
-	Title      string   `json:"title" table:"Title"`
-	Key        string   `json:"key" table:"Key"`
-	Type       string   `json:"type" table:"Type"`
-	PictureURL string   `json:"pic,omitempty"`
-	Primary    bool     `json:"is_primary" table:"Primary"`
-	Order      int      `json:"order"`
-	TopicPools []string `json:"topic_pools,omitempty"`
+	Title      string      `json:"title" table:"Title"`
+	Key        interface{} `json:"key" table:"Key"`
+	Type       string      `json:"type" table:"Type"`
+	PictureURL string      `json:"pic,omitempty"`
+	Primary    bool        `json:"is_primary" table:"Primary"`
+	Order      int         `json:"order"`
+	TopicPools []string    `json:"topic_pools,omitempty"`
+}
+
+func (tc TermCompletion) Keys() []string {
+	switch tc.Key.(type) {
+	case []string:
+		return tc.Key.([]string)
+	default:
+		return []string{tc.Key.(string)}
+	}
 }
 
 type TermCompletions struct {
